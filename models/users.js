57dfd59ace5c759c48388f9a7518ca8af12d170b
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-09-07 22:14:38 +0200

    Finished v1

--- models/users.js
+++ models/users.js
@@ -1,16 +1,63 @@
 const mongoose = require("mongoose");
 
+const firstNameSchema = mongoose.Schema(
+  {
+    boy: {
+      type: [String],
+    },
+    girl: {
+      type: [String],
+    },
+  },
+  { _id: false }
+);
+
+const predictionsSchema = mongoose.Schema(
+  {
+    gender: {
+      type: String,
+      enum: ["boy", "girl", ""],
+      default: "",
+    },
+    firstName: {
+      type: firstNameSchema,
+    },
+    birthDay: {
+      type: Date,
+    },
+  },
+  { _id: false }
+);
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Missing Field"],
+    required: [true, "Missing Name"],
   },
   email: {
     type: String,
-    required: [true, "Missing Field"],
+    required: [true, "Missing E-mail"],
     match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "invalid email address"],
     unique: [true, "User already existing"],
   },
+  password: {
+    type: String,
+    required: [true, "Missing Password"],
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  predictions: {
+    type: predictionsSchema,
+  },
+});
+
+firstNameSchema.pre("save", function (next) {
+  if (!this.boy.length) this.boy = ["", "", ""];
+  if (!this.girl.length) this.girl = ["", "", ""];
+  next();
 });
 
 const User = mongoose.model("users", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,105 @@
 const express = require("express");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/users");
 
-router.post("/", async (req, res) => {
+router.post("/signUp", async (req, res) => {
   try {
-    const user = await new User(req.body).save();
-    res.json({ user });
+    if (req.body.password.length < 6)
+      throw new Error("password must be at least 6 characters");
+    const user = await new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+      token: uid2(32),
+      predictions: {
+        gender: "",
+        firstName: {
+          boy: [],
+          girl: [],
+        },
+
+        birthDay: new Date(2024, 11, 26),
+      },
+    }).save();
+    res.json({ result: true, user });
   } catch (error) {
-    res.json({ error });
+    console.log(error.message);
+
+    res.json({ result: false, error: error.message });
+  }
+});
+
+router.post("/signIn", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw new Error("User not found");
+    if (!bcrypt.compareSync(req.body.password, user.password))
+      throw new Error("Password is incorrect");
+    res.json({ result: true, user });
+  } catch (error) {
+    res.json({ result: false, error: error.message });
+  }
+});
+
+router.get("/:token", async (req, res) => {
+  try {
+    const user = await User.findOne(
+      { token: req.params.token },
+      "name email token predictions"
+    );
+    res.json({ result: true, user });
+  } catch (error) {
+    res.json({ result: false, error: error.message });
+  }
+});
+
+router.put("/updateGender", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { token: req.body.token },
+      { "predictions.gender": req.body.gender },
+      { new: true }
+    );
+    res.json({ result: true, user });
+  } catch (error) {
+    res.json({ result: false, error: error.message });
+  }
+});
+
+router.put("/updateFirstName", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { token: req.body.token },
+      {
+        "predictions.firstName": {
+          boy: req.body.boy,
+          girl: req.body.girl,
+        },
+      },
+      { new: true }
+    );
+    res.json({ result: true, user });
+  } catch (error) {
+    res.json({ result: false, error: error.message });
+  }
+});
+
+router.put("/updateBirthday", async (req, res) => {
+  try {
+    console.log(req.body.date);
+
+    const user = await User.findOneAndUpdate(
+      { token: req.body.token },
+      {
+        "predictions.birthDay": req.body.date,
+      },
+      { new: true }
+    );
+    res.json({ result: true, user });
+  } catch (error) {
+    res.json({ result: false, error: error.message });
   }
 });
 
